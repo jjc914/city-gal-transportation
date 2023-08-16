61108c1778ee394b67bbe397bd6d35a00df82bb9
jjc914 <jjc914@student.cis.edu.hk>
2023-08-16 21:34:46 +0800

    a

--- api/contact-us.js
+++ api/contact-us.js
@@ -11,40 +11,40 @@ async function sendEmail() {
     // Load the service account credentials
     const credentials = require('./developer-395015-1424828da512.json');
   
-    // // Configure the JWT client
-    // const jwtClient = new google.auth.JWT(
-    //   credentials.client_email,
-    //   null,
-    //   credentials.private_key,
-    //   ['https://www.googleapis.com/auth/gmail.send']
-    // );
+    // Configure the JWT client
+    const jwtClient = new google.auth.JWT(
+      credentials.client_email,
+      null,
+      credentials.private_key,
+      ['https://www.googleapis.com/auth/gmail.send']
+    );
   
-    // try {
-    //   // Authorize the client
-    //   await jwtClient.authorize();
+    try {
+      // Authorize the client
+      await jwtClient.authorize();
   
-    //   // Create a new Gmail API client
-    //   const gmail = google.gmail({ version: 'v1', auth: jwtClient });
+      // Create a new Gmail API client
+      const gmail = google.gmail({ version: 'v1', auth: jwtClient });
   
-    //   // Construct the email message
-    //   const email = {
-    //     raw: btoa(
-    //       'From: noreply@developer-395015.iam.gserviceaccount.com\r\n' +
-    //       'To: josh.chasnov@gmail.com\r\n' +
-    //       'Subject: EMAIL_SUBJECT\r\n' +
-    //       'Content-Type: text/plain; charset=utf-8\r\n\r\n' +
-    //       'EMAIL_CONTENT'
-    //     )
-    //   };
+      // Construct the email message
+      const email = {
+        raw: btoa(
+          'From: noreply@developer-395015.iam.gserviceaccount.com\r\n' +
+          'To: josh.chasnov@gmail.com\r\n' +
+          'Subject: EMAIL_SUBJECT\r\n' +
+          'Content-Type: text/plain; charset=utf-8\r\n\r\n' +
+          'EMAIL_CONTENT'
+        )
+      };
   
-    //   // Send the email
-    //   const response = await gmail.users.messages.send({
-    //     userId: 'me',
-    //     requestBody: email
-    //   });
+      // Send the email
+      const response = await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: email
+      });
   
-    //   console.log('Email sent:', response.data);
-    // } catch (error) {
-    //   console.error('Error sending email:', error);
-    // }
+      console.log('Email sent:', response.data);
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
   }
