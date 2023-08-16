2b1095a0850ed40d4cd7b1ab53648fccb2973ce2
jjc914 <jjc914@student.cis.edu.hk>
2023-08-16 21:25:57 +0800

    a

--- api/contact-us.js
+++ api/contact-us.js
@@ -4,4 +4,47 @@ const { google } = require('googleapis');
 export default function contactUs(req, res) {
     res.statusCode = 200;
     res.json({ message: 'AHHHH' });
+    sendEmail();
 }
+
+async function sendEmail() {
+    // Load the service account credentials
+    const credentials = require('/api/developer-395015-1424828da512.json');
+  
+    // Configure the JWT client
+    const jwtClient = new google.auth.JWT(
+      credentials.client_email,
+      null,
+      credentials.private_key,
+      ['https://www.googleapis.com/auth/gmail.send']
+    );
+  
+    try {
+      // Authorize the client
+      await jwtClient.authorize();
+  
+      // Create a new Gmail API client
+      const gmail = google.gmail({ version: 'v1', auth: jwtClient });
+  
+      // Construct the email message
+      const email = {
+        raw: btoa(
+          'From: YOUR_EMAIL_ADDRESS\r\n' +
+          'To: RECIPIENT_EMAIL_ADDRESS\r\n' +
+          'Subject: EMAIL_SUBJECT\r\n' +
+          'Content-Type: text/plain; charset=utf-8\r\n\r\n' +
+          'EMAIL_CONTENT'
+        )
+      };
+  
+      // Send the email
+      const response = await gmail.users.messages.send({
+        userId: 'me',
+        requestBody: email
+      });
+  
+      console.log('Email sent:', response.data);
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  }
