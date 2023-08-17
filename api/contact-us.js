4b196aef364145d1661a0d900397056115a2c96f
Joshua Chasnov <jjc914@student.cis.edu.hk>
2023-08-17 15:45:31 +0800

    a

--- api/contact-us.js
+++ api/contact-us.js
@@ -2,32 +2,33 @@ const nodemailer = require('nodemailer');
 
 // api/contact-us
 export default async function contactUs(req, res) {
+    const { body } = req;
     res.statusCode = 200;
-    res.json({ message: req });
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_ADDRESS,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    });
+    res.json({ message: body });
+    // const transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: process.env.EMAIL_ADDRESS,
+    //         pass: process.env.EMAIL_PASSWORD,
+    //     },
+    // });
 
-    const mailOptions = {
-        from: process.env.EMAIL_ADDRESS,
-        to: 'josh.chasnov@gmail.com',
-        subject: 'EMAIL_SUBJECT',
-        text: 'EMAIL_CONTENT',
-    };
+    // const mailOptions = {
+    //     from: process.env.EMAIL_ADDRESS,
+    //     to: 'josh.chasnov@gmail.com',
+    //     subject: 'EMAIL_SUBJECT',
+    //     text: 'EMAIL_CONTENT',
+    // };
       
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            console.error('Error sending email:', error);
-            res.statusCode = 200;
-            res.json({ message: error });
-        } else {
-            console.log('Email sent:', info.response);
-            res.statusCode = 200;
-            res.json({ message: 'email sent' });
-        }
-    });
+    // transporter.sendMail(mailOptions, function(error, info) {
+    //     if (error) {
+    //         console.error('Error sending email:', error);
+    //         res.statusCode = 200;
+    //         res.json({ message: error });
+    //     } else {
+    //         console.log('Email sent:', info.response);
+    //         res.statusCode = 200;
+    //         res.json({ message: 'email sent' });
+    //     }
+    // });
 }
