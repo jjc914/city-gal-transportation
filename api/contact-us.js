51911c6234395d320f26d809e0421571aa780bf8
jjc914 <jjc914@student.cis.edu.hk>
2023-08-17 22:15:59 +0800

    a

--- api/contact-us.js
+++ api/contact-us.js
@@ -11,34 +11,45 @@ export default async function contactUs(req, res) {
             res.setHeader('Content-Type', 'application/json');
             res.json({ message: body });
 
-            // const transporter = nodemailer.createTransport({
-            //     service: 'gmail',
-            //     auth: {
-            //         user: process.env.EMAIL_ADDRESS,
-            //         pass: process.env.EMAIL_PASSWORD,
-            //     },
-            // });
-        
-            // const mailOptions = {
-            //     from: process.env.EMAIL_ADDRESS,
-            //     to: 'josh.chasnov@gmail.com',
-            //     subject: 'EMAIL_SUBJECT',
-            //     text: 'EMAIL_CONTENT',
-            // };
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.EMAIL_ADDRESS,
+                    pass: process.env.EMAIL_PASSWORD,
+                },
+            });
+            
+            const content = 'First name: ' + body['first-name'] + '\n' +
+                            'Last name: ' + body['last-name'] + '\n' +
+                            'Phone number: ' + body['phone-number'] + '\n' +
+                            'Email: ' + body['email'] + '\n' +
+                            'Pick up: ' + body['pick-up'] + '\n' +
+                            'Drop off: ' + body['drop-off'] + '\n' +
+                            'Flight: ' + body['flight'] + '\n' +
+                            'Is rideshare: ' + body['isRideshare'] + '\n' +
+                            'Body: ' + body['body'];
+
+
+            const mailOptions = {
+                from: process.env.EMAIL_ADDRESS,
+                to: 'josh.chasnov@gmail.com',
+                subject: 'Submitted Contact Us Form',
+                text: content,
+            };
               
-            // transporter.sendMail(mailOptions, function(error, info) {
-            //     if (error) {
-            //         console.error('Error sending email: ', error);
-            //         res.statusCode = 200;
-            //         res.setHeader('Content-Type', 'application/json');
-            //         res.json({ message: error });
-            //     } else {
-            //         console.log('Email sent: ', info.response);
-            //         res.statusCode = 200;
-            //         res.setHeader('Content-Type', 'application/json');
-            //         res.json({ message: 'Email sent successfully. ' });
-            //     }
-            // });
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    console.error('Error sending email: ', error);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json({ message: error });
+                } else {
+                    console.log('Email sent: ', info.response);
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json({ message: 'Email sent successfully. ' });
+                }
+            });
         });
     } else if (req.method === 'GET') {
         res.statusCode = 404;
