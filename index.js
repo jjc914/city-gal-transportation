607ff9ca955a934482fd7e75216afbe557f89e11
Joshua Chasnov <jjc914@student.cis.edu.hk>
2023-08-17 16:02:32 +0800

    test

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const ROOT_URL = "https://city-gal-transportation.vercel.app";
+const ROOT_URL = 'https://city-gal-transportation.vercel.app';
 var navOpen = false;
 
 function toggleNav() {
     if (!navOpen) {
-        document.getElementById("nav-overlay").style.height = "100%";
-        document.body.style.height = "100%";
-        document.body.style.overflowY = "hidden";
-        document.getElementById("nav-overlay").style.paddingTop = "100px";
+        document.getElementById('nav-overlay').style.height = '100%';
+        document.body.style.height = '100%';
+        document.body.style.overflowY = 'hidden';
+        document.getElementById('nav-overlay').style.paddingTop = '100px';
 
     } else {
-        document.getElementById("nav-overlay").style.height = "0%";
+        document.getElementById('nav-overlay').style.height = '0%';
         document.body.style.height = null;
         document.body.style.overflowY = null;
-        document.getElementById("nav-overlay").style.paddingTop = "0px";
+        document.getElementById('nav-overlay').style.paddingTop = '0px';
     }
 
-    Array.prototype.slice.call(document.getElementById("hamburger-nav").children).forEach(element => {
-        if (element.classList.contains("hidden")) {
-            element.classList.remove("hidden");
+    Array.prototype.slice.call(document.getElementById('hamburger-nav').children).forEach(element => {
+        if (element.classList.contains('hidden')) {
+            element.classList.remove('hidden');
         } else {
-            element.classList.add("hidden");
+            element.classList.add('hidden');
         }
     });
     navOpen = !navOpen;
 }
 
 window.onload = function() {
-    element = document.getElementById("phone-input");
-    element.addEventListener("keydown", function(event) {
+    element = document.getElementById('phone-input');
+    element.addEventListener('keydown', function(event) {
         if (event.key != 'Backspace' && (element.value.length === 3 || element.value.length === 7)){
             element.value += '-';
         }
@@ -40,34 +40,44 @@ window.onload = function() {
 
 // https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
 function setInputFilter(textbox, inputFilter) {
-    [ "input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout" ].forEach(function(event) {
+    [ 'input', 'keydown', 'keyup', 'mousedown', 'mouseup', 'select', 'contextmenu', 'drop', 'focusout' ].forEach(function(event) {
         textbox.addEventListener(event, function(e) {
             if (inputFilter(this.value)) {
                 // Accepted value.
                 this.oldValue = this.value;
                 this.oldSelectionStart = this.selectionStart;
                 this.oldSelectionEnd = this.selectionEnd;
-            } else if (this.hasOwnProperty("oldValue")) {
+            } else if (this.hasOwnProperty('oldValue')) {
                 // Rejected value: restore the previous one.
                 this.value = this.oldValue;
                 this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
             } else {
                 // Rejected value: nothing to restore.
-                this.value = "";
+                this.value = '';
             }
         });
     });
 } 
 
 function onSendContactUs() {
-    fetch(ROOT_URL + '/api/contact-us')
+    const url = `${ROOT_URL}/api/endpoint`;
+    const data = {
+        name: 'John Doe',
+        email: 'johndoe@example.com'
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
     .then(response => response.json())
-    .then(data => {
-    // Process the response data
-        console.log(data);
+    .then(responseData => {
+        console.log('Response:', responseData);
     })
     .catch(error => {
-        // Handle any errors
         console.error('Error:', error);
     });
 }
